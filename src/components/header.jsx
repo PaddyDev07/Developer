74715afc16af7f7b49d6828e54c7577148c15f69
Paddy <zanwarpandurang365gmail.com>
2022-07-26 22:55:07 +0530

    use statehook added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () =>{
-    return <h1>Hello react</h1>
+    const [number, setNumber] = useState(0)
+
+    return (
+        <>
+        <h1>Hello react</h1>
+        <h5>{number}</h5>
+        <button onClick={()=> setNumber(number + 1)}>Click to add number(+)</button>
+        <button onClick={()=> setNumber(number + 1)} style={{padding:20, margin:20}}>Click to add number</button>
+        </>
+    )
 }
 
 export default Header;
