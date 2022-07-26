298af23f0b53e24fd6bec806e78e4cd99da386bf
Paddy <zanwarpandurang365gmail.com>
2022-07-26 22:07:44 +0530

    header component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import {header} from './components/header'
 
 function App() {
   return (
    <>
-   <div>Comming soon...</div>
+   <header></header>
    </>
   );
 }
